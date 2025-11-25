6a2a202c596022c160e59ac0afb8ced19fa029da
Léon Dubau <leondubau@MacBook-Air-de-Leon.local>
2025-11-25 12:57:50 +0100

    valgrind a verif

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: leondubau <leondubau@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 12:41:43 by ldubau            #+#    #+#             */
-/*   Updated: 2025/11/23 11:37:27 by leondubau        ###   ########.fr       */
+/*   Updated: 2025/11/24 23:02:27 by leondubau        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 3
+# define BUFFER_SIZE 100
 # endif
 
 size_t	len_stock(const char *s)
@@ -25,6 +25,8 @@ size_t	len_stock(const char *s)
 
 	i = 0;
 	while (s[i] != '\0' && s[i] != '\n')
+		i ++;
+	if (s[i] == '\n')
 		i ++;
 	return (i);
 }
@@ -66,20 +68,20 @@ char	*my_strjoin(const char *s1, const char *s2)
 	j = 0;
 	if (!s1 || !s2)
 		return (NULL);
-	str = malloc(sizeof(char) * (len_stock(s1) + len_stock(s2)) + 1);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
 	if (!str)
 		return (NULL);
-	while (s1[i] && s1[i] != '\n')
+	while (s1[i])
 	{
 		str[i] = s1[i];
 		i ++;
 	}
-	while (s2[j] && s2[j] != '\n')
+	while (s2[j])
 	{
 		str[i + j] = s2[j];
 		j ++;
 	}
-	str[i + j] = '\n';
+	str[i + j] = '\0';
 	return (str);
 }
 
@@ -103,71 +105,79 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
+char	*write_line(char *str)
+{
+	int		size;
+	char	*res;
+	int		i;
+
+	i = 0;
+	size = len_stock(str);
+	res = malloc(sizeof(char) * size + 1);
+	while(i < size)
+	{
+		res[i] = str[i];
+		i ++;
+	}
+	res[i] = '\0';
+	return (res);
+}
 
 char	*get_next_line(int fd)
 {
-	static char		buf[BUFFER_SIZE];
-	char	*line;
+	char		buf[BUFFER_SIZE + 1];
+	char		*line;
+	static char	*stock;
 	char		*tmp;
 	int			size_read;
-	int			x;
 
-	x = 0;
 	size_read = 1;
-	line = ft_strdup("");
-	if (!line)
+	if (!stock)
+		stock = ft_strdup("");
+	if (!stock)
 		return (NULL);
-	if (buf[0] != '\0')
+	while (size_read > 0 && !ft_strchr(stock, '\n'))
 	{
-		line = ft_strdup(ft_strchr(buf, '\n') + 1);
-		x = 1;
-	}
-	while (size_read > 0 && (!ft_strchr(buf, '\n') || x == 1))
-	{
-		x = 0;
 		size_read = read(fd, buf, BUFFER_SIZE);
-		if (size_read < BUFFER_SIZE)
-			buf[size_read] = '\0';
-		tmp = my_strjoin(line, buf);
-		free(line);
+		if (size_read < 0)
+			break;
+		buf[size_read] = 0;
+		tmp = my_strjoin(stock, buf);
+		free(stock);
 		if (!tmp)
 			return (NULL);
-		line = tmp;
+		stock = tmp;
+	}
+	line = write_line(stock);
+	if (ft_strchr(stock, '\n'))
+	{
+		tmp = ft_strdup(ft_strchr(stock, '\n') + 1);
+		free(stock);
+		stock = tmp;
 	}
 	return (line);
 }
 
 int    main(void)
 {
-    char    *line;
-    int        i;
-    int        fd;
-    fd = open("text.txt", O_RDONLY);
-    i = 1;
-    while (i < 19)
-    {
-        line = get_next_line(fd);
-        printf("line [%02d]: %s", i, line);
-        free(line);
-        i++;
-    }
+	char	*line;
+	int		i;
+	int		fd;
+	fd = open("text.txt", O_RDONLY);
+	i = 1;
+	while (i < 14)
+	{
+		line = get_next_line(fd);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
 	if (i == -1)
 	{
 		printf("<ERROR>\n");
 		close(fd);
 		return (-1);
 	}
-    close(fd);
-    return (0);
+	close(fd);
+	return (0);
 }
-
-// int main(void)
-// {
-// 	int fd;
-// 	fd = open("text.txt", O_RDONLY);
-// 	if (fd != -1)
-// 		printf("%s", get_next_line(fd));
-// 		printf("%s", get_next_line(fd));
-// 		// printf("%s", get_next_line(fd));
-// 	return 0;
-// }
